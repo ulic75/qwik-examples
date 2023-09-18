7a5e03ffacd6748db9b5fd9a7cdc93120db94adb
ulic75 <ulic75@gmail.com>
2023-09-18 15:32:28 -0600

    fix(authjs/cred-github-google): remove console.log

--- authjs/creds-github-google/src/routes/protected/index.tsx
+++ authjs/creds-github-google/src/routes/protected/index.tsx
@@ -4,7 +4,6 @@ import type { RequestHandler } from '@builder.io/qwik-city'
 
 export const onRequest: RequestHandler = (event) => {
   const session: Session | null = event.sharedMap.get('session')
-  console.log(session)
   if (!session || new Date(session.expires) < new Date())
     throw event.redirect(302, `/api/auth/signin?callbackUrl=${encodeURIComponent(event.url.href)}`)
 }
